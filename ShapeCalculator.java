d80c6a712f565a92768bd56e71260fbea829ac6b
TryggerVista <150004167@vistausd.org>
2025-08-19 15:22:45 -0700

    Create ShapeCalculator.java

---
+++ ShapeCalculator.java
@@ -0,0 +1,23 @@
+package shapecalculator;
+import java.util.Scanner;
+
+public class ShapeCalculator {
+
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      
+      //[[Create a Sqaure]]\\
+      
+      int SquareSideLength;
+      int SquareArea;
+      
+      System.out.print("What is the side length of the sqaure ");
+      SquareSideLength = input.nextInt();
+      
+      SquareArea = SquareSideLength * SquareSideLength;
+      
+      System.out.println("A sqaure with side length "+SquareSideLength+" is "+SquareArea);
+      
+    }
+    
+}
